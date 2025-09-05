3c19131efa4c74478161137393f1be78ee0fa7d1
Ignatius <ignatiusx47@gmail.com>
2025-09-05 15:26:30 +0300

    Update

--- music-app/client/vite.config.js
+++ music-app/client/vite.config.js
@@ -3,16 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'https://music-player-ypel.onrender.com',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  },
   build: {
     outDir: 'dist'
   }
