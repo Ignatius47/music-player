ad5c1056111153b1262454c16cdbc77be4fb11d6
Ignatius <ignatiusx47@gmail.com>
2025-09-05 08:59:16 +0300

    Update

--- music-app/client/vite.config.js
+++ music-app/client/vite.config.js
@@ -1,18 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    proxy: mode === 'development'
-      ? {
-          '/api': {
-            target: 'http://localhost:4000',
-            changeOrigin: true,
-            secure: false,
-          },
-        }
-      : undefined,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
   },
-}))
+  build: {
+    outDir: 'dist'
+  }
+})
